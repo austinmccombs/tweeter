e068ed24d9f363e1d7b5ba441b73c70ae922ca3f
Cortney Gardner <cortneygardner@PC-LM31571.overstock.com>
2018-09-10 15:38:39 -0600

    bios_controller

--- app/controllers/bios_controller.rb
+++ app/controllers/bios_controller.rb
@@ -1,4 +1,5 @@
 class BiosController < ApplicationController
+  before_action :set_bio, only: [:update, :edit, :show]
   def index
   end
 
@@ -13,17 +14,40 @@ class BiosController < ApplicationController
   end
 
   def update
+    @bio = Bio.find(params[:id])
+    bio.user_id = current_user.id
+    if @bio.update(bio_params)
+      redirect_to @bio
+    else
+      redirect_to :edit
+    end
   end
 
   def destroy
   end
 
   def show
+    @user = current_user
+    @bio = Bio.find(params[:id])
   end
 
   def edit
+    @bio = Bio.find(params[:id])
+    bio.user_id = current_user.id
+    render partial: "form"
   end
 
   def new
+    @bio = Bio.new
+    render partial: "form"
+  end
+
+  private
+  def bio_params
+    params.require(:bio).permit(:profile_image, :description)
+  end
+
+  def set_bio
+    @bio = current_user.bios.find(params[:bio_id])
   end
 end
